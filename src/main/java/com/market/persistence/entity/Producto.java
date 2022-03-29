72b9086c6f32300824c1b38d5560a17fcb935b6c
Camilo Castro <camilo.castrotech@gmail.com>
2022-03-29 14:18:25 -0500

    Se agrego la relación muchos a uno de la entiendad Producto con la entidad Categoria

--- src/main/java/com/market/persistence/entity/Producto.java
+++ src/main/java/com/market/persistence/entity/Producto.java
@@ -30,6 +30,10 @@ public class Producto {
 
     private Boolean estado;
 
+    @ManyToOne
+    @JoinColumn(name = "id_categoria",insertable = false, updatable = false)
+    private Categoria categoria;
+
 
 
     public Long getIdProduto() {
